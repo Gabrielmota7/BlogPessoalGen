6bacf52efa3117bbb0ff97b289c5ae01ffe47750
gabrielmota7 <motagabriel819@gmail.com>
2021-06-11 17:26:21 -0300

    Trocando Main

--- src/main/java/org/generation/blogPessoal/BlogPessoalApplication.java
+++ src/main/java/org/generation/blogPessoal/BlogPessoalApplication.java
@@ -2,6 +2,7 @@ package org.generation.blogPessoal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +14,7 @@ import org.springframework.web.servlet.ModelAndView;
 public class BlogPessoalApplication {
 	
 	
-	@PostMapping
+	@GetMapping
 	public ModelAndView swaggerUi() {
 	return new ModelAndView("redirect:/swagger-ui/");
 	}
